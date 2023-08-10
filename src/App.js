b6bbe51636431fb51d349be8a8a5e6fec290358a
doaaabdelahdi <doaaabdelhadi22@gmail.com>
2023-08-11 01:28:18 +0300

    DoaaAbdelahdiWebsite

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-
-function App() {
+import Bio from "./components/Bio";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
+import Elevator from "./components/Elevator";
+function App() { 
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Home />
+      <Bio />
+      <Skills />
+      <Portfolio />
+      <Elevator/>
+      <Contact />
     </div>
   );
 }
-
 export default App;
